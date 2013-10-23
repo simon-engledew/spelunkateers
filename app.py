cddc4ead0250d1e5309fa66df677fd4422709381
Simon Engledew <simon.engledew@gmail.com>
2013-10-23 22:20:05 +0100

    tided up a bit

--- app.py
+++ app.py
@@ -16,25 +16,29 @@ class DateEncoder(json.JSONEncoder):
             return to_isodate(datetime.datetime.fromtimestamp(time.mktime(o)))
         return json.JSONEncoder.default(self, o)
 
-app = flask.Flask(__name__, static_url_path='')
+app = flask.Flask(__name__)
 app.json_encoder = DateEncoder
 
 def token(bytes=32):
     return base64.urlsafe_b64encode(os.urandom(bytes))
 
+def static(path):
+    return flask.url_for('static', filename=path, cache=str(int(os.stat(os.path.join(app.static_folder, path)).st_mtime)))
+
 @app.context_processor
 def functions():
-    return {'token': token}
+    return {'token': token, 'static': static}
 
 @app.route('/')
 def root():
     return flask.render_template('index.html')
 
+def data():
+    return feedparser.parse('http://gdata.youtube.com/feeds/base/users/Spelunkateers/uploads?alt=rss&v=2&orderby=published')
+
 @app.route('/feed.json')
 def feed():
-    data = feedparser.parse('http://gdata.youtube.com/feeds/base/users/Spelunkateers/uploads?alt=rss&v=2&orderby=published')
-    # return flask.json.dumps(data)
-    return flask.Response('var data = {0};'.format(json.dumps(data, cls=DateEncoder)), mimetype='application/json')
+    return flask.Response('var data = {0};'.format(json.dumps(data(), cls=DateEncoder)), mimetype='application/json')
 
 if __name__ == "__main__":
     app.debug = True
